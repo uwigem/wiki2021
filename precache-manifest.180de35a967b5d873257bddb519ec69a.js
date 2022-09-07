94ded85ddcf77133cf98103d5dcd302332b50539
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-09-06 19:28:37 -0700

    Updates

---
+++ precache-manifest.180de35a967b5d873257bddb519ec69a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445e7a07ab338d2c40c45f2dc7582371",
+    "url": "/MilkyTea123.github.io/index.html"
+  },
+  {
+    "revision": "d6a31694bf4773ddd32f",
+    "url": "/MilkyTea123.github.io/static/css/main.css"
+  },
+  {
+    "revision": "d6a31694bf4773ddd32f",
+    "url": "/MilkyTea123.github.io/static/js/main.js"
+  },
+  {
+    "revision": "47a6bb091aa459578b95cd90574064e9",
+    "url": "/MilkyTea123.github.io/static/js/main.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dc85c7cf388ed560ed3294f2c3d1450.js"
+  "/MilkyTea123.github.io/precache-manifest.180de35a967b5d873257bddb519ec69a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MilkyTea123.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
