9665dddc6bb63bf8d625c4491f420c1461caf6dd
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-09-06 19:14:51 -0700

    Updates

---
+++ precache-manifest.4dc85c7cf388ed560ed3294f2c3d1450.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61d0af2d8c88609fe14d98705335e86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "447ff7f5e3e2cfd7101b",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "447ff7f5e3e2cfd7101b",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "47a6bb091aa459578b95cd90574064e9",
+    "url": "/static/js/main.js.LICENSE.txt"
+  }
+]);
